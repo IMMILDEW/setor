a8bb9ca441e6c241afd56f24b8bdc9136f01b7f8
agusmakmun <summon.agus@gmail.com>
2018-05-15 09:34:17 +0700

    fixed ssl

--- setor/setor.py
+++ setor/setor.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import ssl
 import sys
 import time
 import random
@@ -15,18 +16,17 @@ from logging import handlers
 
 import userAgents
 
-import os, ssl
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-    getattr(ssl, '_create_unverified_context', None)): 
+        getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
-    
+
 # Path to save the logs.
 # If not exists, doing to create.
 log_path = '/var/logs/setor'
 if not os.path.exists(log_path):
     os.makedirs(log_path)
 
-## Terminal Colors
+# Terminal Colors
 RED = '\033[91m'
 CYAN = '\033[96m'
 GREEN = '\033[92m'
@@ -42,9 +42,8 @@ URL_TO_CHECK_PUBLIC_IP = [
 
 # Chcecking privillage
 if os.getuid() != 0:
-    print "\n{}[-] Please Access as root..!{}\n".format(RED, END_COLOR)
+    print("\n{}[-] Please Access as root..!{}\n".format(RED, END_COLOR))
     sys.exit()
-else: pass
 
 
 def Banner():
@@ -102,59 +101,48 @@ def visitLink(link, log_choice, host_conf, port_conf):
         opener = urllib2.build_opener()
         opener.addheaders = [('User-agent', random.choice(userAgents.userAgents))]
 
-        print "{}[i]{} Checking your public IP...".format(CYAN, END_COLOR)
+        print ("{}[i]{} Checking your public IP...".format(CYAN, END_COLOR))
         checking_ip = opener.open(random.choice(URL_TO_CHECK_PUBLIC_IP)).read().replace('\n', '')
-        print "{}[i]{} IP now   - {}".format(CYAN, END_COLOR, checking_ip) #Checking new IP
+        print ("{}[i]{} IP now   - {}".format(CYAN, END_COLOR, checking_ip))  # Checking new IP
         time.sleep(0.5)
 
-        print "{}[i]{} Visit to - {}".format(CYAN, END_COLOR, link)
-        opener.open(link)                                                  #Opening the link
-        print "{}[i] Success visited!{}".format(GREEN, END_COLOR)
+        print ("{}[i]{} Visit to - {}".format(CYAN, END_COLOR, link))
+        opener.open(link)  # Opening the link
+        print ("{}[i] Success visited!{}".format(GREEN, END_COLOR))
 
         if log_choice == True:
             log_logger.info("IP: {} - Succes visited to: {}".format(checking_ip, link))
 
-    except urllib2.HTTPError as e:
-        print "{}[i] {} - {}{}".format(YELLOW, e, link, END_COLOR)
+    except Exception as e:
+        print ("{}[i] {} - {}{}".format(YELLOW, e, link, END_COLOR))
         if log_choice == True:
             log_logger.info("{} - {}".format(e, link))
 
-    except urllib2.URLError as e:
-        print "{}[i] {} - {}{}".format(YELLOW, e, link, END_COLOR)
-        if log_choice == True:
-            log_logger.info("{} - {}".format(e, link))
-
-    except httplib.HTTPException as e:
-        print "{}[i] {} - {}{}".format(YELLOW, e, link, END_COLOR)
-        if log_choice == True:
-            log_logger.info("{} - {}".format(e, link))
-
-    except socks.SOCKS4Error as e:
-        print "{}[i] {} - {}{}".format(YELLOW, e, link, END_COLOR)
-        if log_choice == True:
-            log_logger.info("{} - {}".format(e, link))
-    
-    print "{}[i]{} Restating TOR to get new IP...".format(CYAN, END_COLOR)
+    print ("{}[i]{} Restating TOR to get new IP...".format(CYAN, END_COLOR))
     time.sleep(0.5)
 
-    #Restarting the TOR to get new IP, this bassed for Ubuntu. other linux use different method.
+    # Restarting the TOR to get new IP, this bassed for Ubuntu. other linux use different method.
     os.system("sudo service tor restart")
 
-    #Random for time wait, just optionally.
+    # Random for time wait, just optionally.
     wait_time = random.randint(10, 15)
-    print "{}[i]{} Wait for {} seconds...".format(CYAN, END_COLOR, wait_time)
+    print ("{}[i]{} Wait for {} seconds...".format(CYAN, END_COLOR, wait_time))
 
-    #Waiting with random between 10 -> 15 seconds from `wait_time`.
+    # Waiting with random between 10 -> 15 seconds from `wait_time`.
     time.sleep(wait_time)
 
 
 def main():
     Banner()
-    print """{} // OPTIONS:{}\n A. Single Link\n B. Multiple Links\n""".format(GREEN, END_COLOR)
+    print ("{} // OPTIONS:{}\n A. Single Link\n B. Multiple Links\n".format(GREEN, END_COLOR))
     choice = raw_input("{}[+]{} Type your choice [a/B]: ".format(GREEN, END_COLOR))
 
-    host_input = raw_input("{}[+]{} Host TOR (Enter to set default: 127.0.0.1): ".format(GREEN, END_COLOR))
-    port_input = raw_input("{}[+]{} Port TOR (Enter to set default: 9050): ".format(GREEN, END_COLOR))
+    host_input = raw_input(
+        "{}[+]{} Host TOR (Enter to set default: 127.0.0.1): ".format(GREEN, END_COLOR)
+    )
+    port_input = raw_input(
+        "{}[+]{} Port TOR (Enter to set default: 9050): ".format(GREEN, END_COLOR)
+    )
 
     host_conf = '127.0.0.1'
     port_conf = 9050
@@ -167,9 +155,13 @@ def main():
     if choice.lower() == 'a':
         inp_single_link = raw_input("{}[+]{} Paste your single link: ".format(GREEN, END_COLOR))
 
-        log_choice = raw_input("{}[+]{} Are you need to save as log? [y/N]: ".format(GREEN, END_COLOR))
-        if log_choice.lower() == 'y': log_choice = True
-        else: log_choice = False
+        log_choice = raw_input(
+            "{}[+]{} Are you need to save as log? [y/N]: ".format(GREEN, END_COLOR)
+        )
+        if log_choice.lower() == 'y':
+            log_choice = True
+        else:
+            log_choice = False
 
         try:
             while True:
@@ -180,11 +172,15 @@ def main():
 
     elif choice.lower() == 'b':
         inp_file_links = raw_input("{}[+]{} Type your file links *.txt: ".format(GREEN, END_COLOR))
-        urls = [ line.strip() for line in open(inp_file_links, 'r') if line.strip() != '' ]
+        urls = [line.strip() for line in open(inp_file_links, 'r') if line.strip() != '']
 
-        log_choice = raw_input("{}[+]{} Are you need to save as log? [y/N]: ".format(GREEN, END_COLOR))
-        if log_choice.lower() == 'y': log_choice = True
-        else: log_choice = False
+        log_choice = raw_input(
+            "{}[+]{} Are you need to save as log? [y/N]: ".format(GREEN, END_COLOR)
+        )
+        if log_choice.lower() == 'y':
+            log_choice = True
+        else:
+            log_choice = False
 
         try:
             while True:
@@ -194,7 +190,8 @@ def main():
             print "{}[-] Finished!{}".format(YELLOW, END_COLOR)
             sys.exit()
 
-    else: print "{}[!] Wrong input!{}".format(RED, END_COLOR)
+    else:
+        print "{}[!] Wrong input!{}".format(RED, END_COLOR)
     logging.shutdown()
 
 if __name__ == '__main__':
